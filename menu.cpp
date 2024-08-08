3fe6fa1864fc4eaeb5d06fd57167d6ada76023ef
Breitfeld Marcel <233168@edu.bbw-hochschule.de>
2024-08-08 18:11:25 +0200

    -work on RenderWIndow

--- menu.cpp
+++ menu.cpp
@@ -7,24 +7,18 @@
 
 Menu::Menu()
 {
-    if (!font.loadFromFile("Fonts/Dimbo Regular.ttf"))
-    {
-        std::cerr << "Fehler beim Laden der Schriftart" << std::endl;
-    }
 
-    // Darstellen der Schrift im Hauptmenü
-    initText(titleText, "SNAKE", 90, gb::colTextOn, sf::Vector2f(gb::winWidth / 2, gb::winHeight * 0.2));             // Initialisieren des Textes "SNAKE", in Abhängigkeit der Fenstergröße
-    initText(startText, "START", 50, gb::colTextOn, sf::Vector2f(gb::winWidth / 2, gb::winHeight * 0.4));             //          -||-             "START",              -||-        
-    initText(highscoreText, "HIGHSCORE", 50, gb::colTextOn, sf::Vector2f(gb::winWidth / 2, gb::winHeight * 0.5));     //          -||-             "HIGHSCORE",          -||-        
-    initText(quitText, "BEENDEN", 50, gb::colTextOn, sf::Vector2f(gb::winWidth / 2, gb::winHeight * 0.6));            //          -||-             "BEENDEN",            -||-        
-
-    selectedItem = 0;   // "START" als ausgewählt festlegen
-    updateTextColors(); // Aufruf der Funktion, welche die einzelnen Menüpunkte, je nach aktueller Auswahl, farbig hinterlegt
 }
 
-void Menu::handleInput(sf::RenderWindow& window)
+void Menu::handleInput(sf::Event& event)
 {
+	if (event.type == sf::Event::KeyPressed)
+	{
+		if (event.key.code == sf::Keyboard::Escape)
+			window.close();
+	}
 }
+
 
 void Menu::update()
 {
@@ -32,12 +26,8 @@ void Menu::update()
 
 void Menu::render(sf::RenderWindow& window)
 {
+	sf::CircleShape shape(50);
+	shape.setFillColor(sf::Color(100, 250, 50));
 }
 
-void Menu::initText(sf::Text& text, const std::string& string, unsigned int size, sf::Color color, sf::Vector2f position)
-{
-}
 
-void Menu::updateTextColors()
-{
-}
